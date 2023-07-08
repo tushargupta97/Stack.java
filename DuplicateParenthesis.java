8d4369c796c63698a57017cf9786235dc6d36802
tushargupta97 <97527569+tushargupta97@users.noreply.github.com>
2023-07-08 17:03:14 +0530

    Add files via upload

---
+++ DuplicateParenthesis.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class DuplicateParenthesis {
+    public static boolean isValid(String str){
+        Stack<Character>s=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){//opening
+                s.push(ch);
+            }
+            else{  //closing
+                if(s.isEmpty()){
+                    return false;
+                }
+                if( (s.peek()=='('&& ch==')')//()
+                   ||(s.peek()=='{'&&ch=='}') //{}
+                   ||(s.peek()=='['&&ch==']'))//[]
+                {
+                    s.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        if(s.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean isDuplicate(String str){
+        Stack<Character>s= new Stack();
+
+        for(int i=0; i<str.length();i++){
+            char ch=str.charAt(i);
+            //closing 
+            if(ch==')'){
+                int count=0;
+                while(!s.isEmpty()&& s.peek()!='('){
+                    s.pop();
+                    count ++;
+                }
+                if(count <1){
+                    return  true; //duplicate
+                }
+                else{
+                    s.pop(); //opening pair
+                }
+               
+            }
+            else{
+                //opening 
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+        
+    public static void main(String args[]){
+        String str1="((a+b))"; //true
+        String str2="(a-b)"; //false
+        System.out.println(isDuplicate(str1));
+        System.out.println(isDuplicate(str2));
+    }
+}
